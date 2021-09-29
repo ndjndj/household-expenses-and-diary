296884b03b8eb08bd610c4dbf961f652d65693a1
ndjndj <teq.ndj@gmail.com>
2021-09-30 02:55:56 +0900

    add event

---
+++ login.js
@@ -0,0 +1,14 @@
+function checkLoginValidation() {
+    return
+}
+
+function addEvent(id, eventType, fn) {
+    document.getElementById(id).addEventListener(eventType, fn);
+  }
+
+document.addEventListener(
+    'load',
+    () => {
+
+    }
+);
