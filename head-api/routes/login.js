81ff2b0a8350354dbc95108c690635823c9b8d34
ndjndj <teq.ndj@gmail.com>
2021-10-05 00:24:33 +0900

    fix

--- head-api/routes/login.js
+++ head-api/routes/login.js
@@ -6,13 +6,13 @@ const pool = require('../db/pool');
 router.post(
     '/',
     function(req, res, next) {
-        const {user_id, pass} = req.body.user;
-
+        const {user_id, password} = req.body.user;
+        console.log(user_id, password);
         pool.query(
             'SELECT ' +
             '    id, user_id, user_name ' +
             'FROM head_users WHERE user_id = $1 AND password =  $2',
-            [user_id, pass],
+            [user_id, password],
             function(error, results) {
                 if(error) { throw error; }
                 console.log(results.rows);
