fb83327c4f1fad720626e594fc765824e2ff9cba
ndjndj <teq.ndj@gmail.com>
2021-10-05 00:03:13 +0900

    id

--- head-api/routes/login.js
+++ head-api/routes/login.js
@@ -6,13 +6,13 @@ const pool = require('../db/pool');
 router.post(
     '/',
     function(req, res, next) {
-        const {id, pass} = req.body.user;
+        const {user_id, pass} = req.body.user;
 
         pool.query(
             'SELECT ' +
             '    id, user_name, password ' +
             'FROM head_users WHERE ($1, $2)',
-            [id, pass],
+            [user_id, pass],
             function(error, results) {
                 if(error) { throw error }
                 console.log(results.rows);
