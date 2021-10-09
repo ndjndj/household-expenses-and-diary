d48e76fac89a7a60c763da7c00123ad1217ffc62
ndjndj <teq.ndj@gmail.com>
2021-10-09 22:25:24 +0900

    unique string

--- common.js
+++ common.js
@@ -1,6 +1,12 @@
 
 const addEvent = (id, eventType, fn) => {
-    document.getElementById(id).addEventListener(eventType, fn);
+    const elem = document.getElementById(id);
+    elem.addEventListener(eventType, fn);
 };
+
+const getUniqueString = (strong=1000, initialString='') => {
+    const now = new Date().getTime().toString(16);
+    return initialString + now + Math.floor(strong * Math.random()).toString;
+}
 
 module.exports = addEvent;
