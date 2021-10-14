06235cc96f4086eea9985f87a5e74ef2ae62af6c
ndjndj <teq.ndj@gmail.com>
2021-10-15 01:01:31 +0900

    getUniqueString

--- categories.js
+++ categories.js
@@ -1,6 +1,10 @@
 function checkValidation(condition, errorRetVal, validRetVal) {
     return condition ? errorRetVal : validRetVal
-  }
+ }
+
+function getUniqueString(strong=1000, initialString='') {
+    return initialString + new Date().getTime().toString(16) + Math.floor(strong * Math.random()).toString(16);
+}
 
 function addData() {
     const seqData = document.getElementById('seq');
@@ -10,5 +14,13 @@ function addData() {
     let message = '';
     message += checkValidation(!seqData, 'seq is null.\n', '');
     message += checkValidation(!categoryData, 'category is null\n', '');
-    
+
+    if (message != '') {
+        window.alert(messsage);
+        return
+    }
+
+    const table = document.getElementById('data-table');
+    let row = table.insertRow(-1);
+    row.id =
 }
