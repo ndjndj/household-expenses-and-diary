ec3770bdf300dd1e9aea695413f81599f42d3fbc
ndjndj <teq.ndj@gmail.com>
2021-10-15 00:58:29 +0900

    validation

--- categories.js
+++ categories.js
@@ -6,5 +6,9 @@ function addData() {
     const seqData = document.getElementById('seq');
     const categoryData = document.getElementById('category');
 
-
+    //check validation
+    let message = '';
+    message += checkValidation(!seqData, 'seq is null.\n', '');
+    message += checkValidation(!categoryData, 'category is null\n', '');
+    
 }
