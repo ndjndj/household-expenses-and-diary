1279c431548ad85d1a68b9df8c63ab2ed805a436
ndjndj <teq.ndj@gmail.com>
2021-10-15 00:56:42 +0900

    checkValidation

---
+++ categories.js
@@ -0,0 +1,10 @@
+function checkValidation(condition, errorRetVal, validRetVal) {
+    return condition ? errorRetVal : validRetVal
+  }
+
+function addData() {
+    const seqData = document.getElementById('seq');
+    const categoryData = document.getElementById('category');
+
+
+}
