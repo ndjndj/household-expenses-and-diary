de52aa20ddbbcf093cc08a7dddb741e967c4691e
ndjndj <teq.ndj@gmail.com>
2021-10-20 23:52:00 +0900

    skelton with categories

--- head-api/routes/categories.js
+++ head-api/routes/categories.js
@@ -3,4 +3,10 @@ const router = express.Router();
 
 const pool = require('../db/pool');
 
-router.post();
+router.post(
+    '/',
+    function(req, res, next) {
+        const {user, datas} = req.body;
+        
+    }
+);
