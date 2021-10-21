f458a3d926367d48fa4ea799f48231eff2d95a34
ndjndj <teq.ndj@gmail.com>
2021-10-22 00:51:14 +0900

    sql

--- head-api/routes/categories.js
+++ head-api/routes/categories.js
@@ -9,8 +9,8 @@ router.post(
     function(req, res, next) {
         const {user, datas} = req.body;
         console.log("func");
-        const sql = 'DELETE head_categories WHERE user_id = $1;'
-        + format('INSERT INTO head_categories (user_id, id, name) VALUES %L', datas['data']);
+        const sql = format('DELETE head_categories WHERE user_id = %I; INSERT INTO head_categories (user_id, id, name) VALUES %L', user.user_id, datas['data']);
+        console.log(sql);
         pool.query(sql, [user.user_id], function(error, results) {
             if(error) { throw error; }
             res.status(201).json({status: 'success'})
