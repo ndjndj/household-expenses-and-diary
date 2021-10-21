7dd65db9742973a642172504936420df2901d352
ndjndj <teq.ndj@gmail.com>
2021-10-22 00:53:38 +0900

    fix sql

--- head-api/routes/categories.js
+++ head-api/routes/categories.js
@@ -9,7 +9,7 @@ router.post(
     function(req, res, next) {
         const {user, datas} = req.body;
         console.log("func");
-        const sql = format('DELETE head_categories WHERE user_id = %I; INSERT INTO head_categories (user_id, id, name) VALUES %L', user.user_id, datas['data']);
+        const sql = format('DELETE FROM head_categories WHERE user_id = %I; INSERT INTO head_categories (user_id, id, name) VALUES %L', user.user_id, datas['data']);
         console.log(sql);
         pool.query(sql, [user.user_id], function(error, results) {
             if(error) { throw error; }
